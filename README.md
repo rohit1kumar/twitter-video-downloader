f79b2deb890ca10b058f26dff6ba2266409bf222
Rohit Kumar <sonu12345kumar12345@gmail.com>
2022-09-17 01:55:42 +0530

    first

---
+++ README.md
@@ -0,0 +1,3 @@
+# Twitter Video Downloader
+
+This is a simple website to download videos from Twitter. It uses the [Twitter API](https://developer.twitter.com/en/docs/twitter-api/tweets/lookup/introduction) to get the video URL and then click to download it.
---
+++ app.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const { Client } = require("twitter-api-sdk");
+require('dotenv').config();
+const ejs = require('ejs');
+
+// Environment variables
+const port = process.env.PORT || 3000;
+const bearerToken = process.env.BEARER_TOKEN;
+
+const app = express();
+
+// Set view engine
+app.set('view engine', 'ejs');
+
+// Parse URL-encoded bodies
+app.use(express.urlencoded({ extended: true }));
+
+// ############################Function to get url of video from twitter#######################
+async function getVideoUrl(TwitterUrl) {
+    const id = TwitterUrl.split("/").pop(); // get status id from url
+    const client = new Client(bearerToken);
+    const response = await client.tweets.findTweetsById({
+        "ids": [
+            id
+        ],
+        "expansions": [
+            "attachments.media_keys"
+        ],
+        "media.fields": [
+            "variants"
+        ]
+    });
+    return response.includes.media[0].variants[1].url;
+}
+
+// index page
+app.get('/', function (req, res) {
+    res.render('index');
+});
+
+// post request to get video url
+app.post('/download', async (req, res) => {
+    try {
+        const url = req.body.url;
+        if (url === undefined || url === null || url === '') {
+            return res.render('error', { error: 'Please enter a valid url' });
+        }
+        const videoUrl = await getVideoUrl(url);
+        return res.render('download', { videoUrl });
+
+    } catch (error) {
+        if (error instanceof TypeError) {
+            return res.render('error', { error: 'Please enter a valid url' });
+        }
+        return res.render('error', { error });    // error page
+    }
+});
+
+app.listen(port, () => console.log(`listening on port ${port}!`));
